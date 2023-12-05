23d9a85ba182b8464d886d33a13ff4c68b0474e5
Nouman Jannat <nouman.jannatiiui@gmail.com>
2023-12-05 16:36:55 +0500

    12/5/2023

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap'
+import Header from './component/Header';
+import {BrowserRouter  , Route, Routes} from 'react-router-dom'
+import Login from "./authentication/Login"
+import Register from "./authentication/Registration"
+import AddProduct from "./product/AddProduct";
+import UpdateProduct from "./product/UpdateProduct";
 function App() {
   //Hellow
   return (
     <div className="App">
-      <button>Normal Button</button>
-      <Button>bootstrap Button</Button>
+      <BrowserRouter>
+      {/* <button>Normal Button</button>
+      <Button>bootstrap Button</Button> */}
+      <Header/>
+      <h1>E-Comm Project</h1>
+      <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/add' element={<AddProduct/>}/>
+      <Route path='/update' element={<UpdateProduct/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,20 @@
+import { Navbar, Nav } from "react-bootstrap";
+import "../App.css";
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto nav-bar-wrapper">
+          <Link to="/add">Add Product</Link>
+          <Link to="/update">Update Product</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
